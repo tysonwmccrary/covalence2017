afb12b43fd5c9b4848deb9ee32a97beb4343a9e6
tysonwmccrary <tysonwmccrary@gmail.com>
2017-09-09 14:04:05 -0400

    repeat song lyrics using javascript

---
+++ covalence-lab-2/js/main.js
@@ -0,0 +1,25 @@
+
+var friends = ['Thomas', 'George', 'Cliff', 'Susan', 'Pat'];
+
+function loop() {
+    for (var j = 0; j < friends.length; j++) {
+        console.log(friends[j]);
+
+        for (var i = 100; i >= 1; i--) {
+            console.log(i + ' ' + 'lines of code in the file,' + ' ' + i + ' ' + 'lines of code,' + ' ' + friends[j] + ' ' + 'strikes one out, clears it all out,' + ' ' + (i - 1) + ' ' + 'lines of code in the file.');
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
