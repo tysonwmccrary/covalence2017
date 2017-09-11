bbf91b542c878d93683e0299c48e1173c6fa4b54
tysonwmccrary <tysonwmccrary@gmail.com>
2017-09-11 13:42:17 -0400

    up date lab 3 and created lab 4

--- covalence-lab-3/js/main.js
+++ covalence-lab-3/js/main.js
@@ -1,47 +1,40 @@
 
-    
 
 
-var friends = ['Thomas', 'George', 'Cliff', 'Susan', 'Pat'];
+
+ friends = ['Thomas', 'George', 'Cliff', 'Susan', 'Pat'];
 
 function loop() {
     for (var j = 0; j < friends.length; j++) {
-        h3.innerHTML = (friends[j]);
-/*
-var div = document.createElement('div');
-div.classname ='friend';
-var h3 = document.createElement('h3');
-var headingText = document.createTextNode(friend)
-h3.appendChild(headingText);
-div.appendChild(h3);
-*/
-        for (var i = 100; i >= 1; i--) {
-            var p = document.createElement('p')
-            p.innerHTML = (i + ' ' + 'lines of code in the file,' + ' ' + i + ' ' + 'lines of code,' + ' ' + friends[j] + ' ' + 'strikes one out, clears it all out,' + ' ' + (i - 1) + ' ' + 'lines of code in the file.');
+        var myFriends = (friends[j]);
 
+        var div = document.createElement('div');
+        div.classname = 'number1';
+        var h3 = document.createElement('h3');
+        var headingText = document.createTextNode(myFriends);
+        h3.appendChild(headingText);
+        div.appendChild(h3);
+
+        for (var i = 100; i >= 0; i--) {
+            var p = document.createElement('p')
+            if (i > 1) {
+                p.innerText = i + ' ' + 'lines of code in the file,' + ' ' + i + ' ' + 'lines of code,' + ' ' + friends[j] + ' ' + 'strikes one out, clears it all out,' + ' ' + (i - 1) + ' ' + 'loose of code in the file.';
+            }
+            else if (i === 1) {
+                p.innerText = i + ' ' + 'line of code in the file,' + ' ' + i + ' ' + 'line of code,' + ' ' + friends[j] + ' ' + 'strikes one out, clears it all out,' + ' ' + (i - 1) + ' ' + 'lines of code in the file.';
+            }
+            else {
+                p.innerText = 'No More line of Code in the File!!!';
+            }
+            div.appendChild(p);
         }
-       // div.appendChild(p);
+        document.body.appendChild(div);
     }
-    //document.body.appendChild(div);
 }
 
 
 
-
-    document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     var button = document.getElementById('home');
-    button.addEventListener('click', loop());
-        var para = document.createElement('p');
-        var text = document.createTextNode('h3');
-
-        para.appendChild(text);
-
-        document.body.appendChild(para);
-    });
-
-
-
-
-
-
-
+    button.addEventListener('click', loop);
+});
---
+++ covalence-lab-4/js/main.js
@@ -0,0 +1,14 @@
+
+var button = document.createElement('button');
+button.innerText('Click to Add Square');
+button.addEventListener('click', change() {
+    document.body.appendChild(button);
+});
+
+var colors = [yellow, orange, red, blue, green, purple];
+for (var i = 0; i < colors.length; i++);
+
+function change() {
+
+
+};
