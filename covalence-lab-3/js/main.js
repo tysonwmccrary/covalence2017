a029e547f0d8ef9a219f3110e8e0aba45190007e
tysonwmccrary <tysonwmccrary@gmail.com>
2017-09-12 09:27:04 -0400

    place song into HTML with new comments

--- covalence-lab-3/js/main.js
+++ covalence-lab-3/js/main.js
@@ -1,20 +1,22 @@
+//Set an array of friends
+friends = ['Thomas', 'George', 'Cliff', 'Susan', 'Pat'];
 
-
-
-
- friends = ['Thomas', 'George', 'Cliff', 'Susan', 'Pat'];
-
+//Make a function to include all things to happen
 function loop() {
+    //loop through the array and select a friend.
     for (var j = 0; j < friends.length; j++) {
         var myFriends = (friends[j]);
-
+        //Create a div to store friend name once selected.
         var div = document.createElement('div');
         div.classname = 'number1';
         var h3 = document.createElement('h3');
+        //Place the name of friend into heading.
         var headingText = document.createTextNode(myFriends);
         h3.appendChild(headingText);
         div.appendChild(h3);
 
+        //Use this loop to add different lines of song when it meets the condintion.
+        //Set p.innerText equal to, so that it is placed inside the created p tag.
         for (var i = 100; i >= 0; i--) {
             var p = document.createElement('p')
             if (i > 1) {
@@ -33,7 +35,7 @@ function loop() {
 }
 
 
-
+//This eventListner is for the whole document.
 document.addEventListener('DOMContentLoaded', function () {
     var button = document.getElementById('home');
     button.addEventListener('click', loop);
